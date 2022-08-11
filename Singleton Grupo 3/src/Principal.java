039998533943996e13903957ec065ba9280d6914
Tomy0032 <tomas.com9@gmail.com>
2022-08-10 23:12:23 -0300

    Clase Principal. Segundo commit Tomás

---
+++ Singleton Grupo 3/src/Principal.java
@@ -0,0 +1,15 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Casino casino = Casino.createCasino();
+		
+		casino.setEncargado(new Persona("31234567","Fructuosa Mora",Status.ACTIVO));		
+		casino.setNombre("Las Garzas");
+		
+		System.out.println(casino.toString());
+
+	}
+
+}
